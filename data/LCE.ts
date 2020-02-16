c89eb924f158c6b3ecbf4ce149ceafe01fdda400
rochellev <rochellevroberts@utexas.edu>
2020-02-16 12:06:53 -0800

    add LCE.ts only comments so far

---
+++ data/LCE.ts
@@ -0,0 +1,8 @@
+/* 
+    LCE provides wrappers for representing data that is loaded asynchronously.
+    Loading - Data that has been requested, and is in loading state. Can also hold previousData.
+    Content - Data that loaded successfully loaded, has a data property.
+    Error - Data that failed to load with an error. Has the error object, and potentially previousData.
+*/
+
+
